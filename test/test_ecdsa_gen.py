a77a8ce411bc2c75c7350b651487597c15069e8b
-TOSH- <tosh@t0x0sh.org>
2018-08-29 17:11:06 +0200

    Add tests (FIPS-186-4) for ECDSA signature generation

---
+++ test/test_ecdsa_gen.py
@@ -0,0 +1,64 @@
+
+import re
+import sys
+
+sys.path.insert(0, './src/')
+
+from ecdsa import *
+
+# FIPS 186-4 tests vectors for signature generation
+TEST_FILE = "test/SigGen.txt"
+
+
+if __name__ == '__main__':
+    params = {}
+    hashs = {}
+    curves = {}
+
+    hashs['SHA-224'] = hashlib.sha224
+    hashs['SHA-256'] = hashlib.sha256
+    hashs['SHA-384'] = hashlib.sha384
+    hashs['SHA-512'] = hashlib.sha512
+
+    curves['P-521'] = ECDSAParamsP521()
+
+    line_num = 0
+
+    with open(TEST_FILE, 'r') as fd:
+        for line in fd:
+            line_num += 1
+            line = line.replace("\n", "").replace("\r", "")
+            if len(line) > 0 and line[0] != '#':
+
+                m = re.search('^\[(\S+),(\S+)\]$', line)
+                if m is not None:
+                    params['Curve'] = m.group(1)
+                    params['Hash'] = str(m.group(2))
+
+                m = re.search('^(\S+) = (\S+)$', line)
+                if m is not None:
+                    params[m.group(1)] = m.group(2)
+
+                    if m.group(1) == 'S':
+                        if params['Curve'] in curves:
+                            if params['Hash'] in hashs:
+                                curve = curves[params['Curve']]
+                                curve.hash_fct = hashs[params['Hash']]
+
+                                sys.stdout.write("Testing %s - %s (line %d)..." % (params['Curve'], params['Hash'], line_num))
+                                sys.stdout.flush()
+
+                                (public, private) = curve.genKeys()
+
+                                private.d = int(params['d'], 16)
+                                public.p = private.d * public.params.generator
+
+                                assert public.p.x == int(params['Qx'], 16)
+                                assert public.p.y == int(params['Qy'], 16)
+
+                                s = private.sign(bytes.fromhex(params['Msg']), int(params['k'], 16))
+
+                                assert s.r == int(params['R'], 16)
+                                assert s.s == int(params['S'], 16)
+
+                                sys.stdout.write("OK\n")
